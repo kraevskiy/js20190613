e152ba4ebe8649066e1ffec398f5b0db9e374ccb
Stanislav Gavrylov <stasgavrylov@gmail.com>
2019-08-05 23:18:41 +0300

    Adds coins price fetching

--- scripts/services/DataService.js
+++ scripts/services/DataService.js
@@ -1,108 +1,72 @@
-const data = [
-  {
-    "id": "btc-bitcoin",
-    "name": "Bitcoin",
-    "symbol": "BTC",
-    "rank": 1,
-    "is_new": false,
-    "is_active": true,
-    "type": "coin",
-    "price": 1000
+const COINS_URL = 'https://api.coinpaprika.com/v1/coins';
+const getSingleCoinUrl = id => `${COINS_URL}/${id}/ohlcv/latest`;
+
+
+const HttpService = {
+  sendRequest(url, successCallback, method = 'GET') {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(method, url);
+
+    xhr.send();
+
+    xhr.onload = () => {
+      if (xhr.status != 200) {
+        alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+      } else {
+        let responseData = JSON.parse(xhr.responseText);
+        successCallback(responseData);
+      }
+    }
   },
-  {
-    "id": "eth-ethereum",
-    "name": "Ethereum",
-    "symbol": "ETH",
-    "rank": 2,
-    "is_new": false,
-    "is_active": true,
-    "type": "coin",
-    "price": 900
-  },
-  {
-    "id": "xrp-xrp",
-    "name": "XRP",
-    "symbol": "XRP",
-    "rank": 3,
-    "is_new": false,
-    "is_active": true,
-    "type": "coin",
-    "price": 800
-  },
-  {
-    "id": "eos-eos",
-    "name": "EOS",
-    "symbol": "EOS",
-    "rank": 4,
-    "is_new": false,
-    "is_active": true,
-    "type": "coin",
-    "price": 700
-  },
-  {
-    "id": "ltc-litecoin",
-    "name": "Litecoin",
-    "symbol": "LTC",
-    "rank": 5,
-    "is_new": false,
-    "is_active": true,
-    "type": "coin",
-    "price": 600
-  },
-  {
-    "id": "bch-bitcoin-cash",
-    "name": "Bitcoin Cash",
-    "symbol": "BCH",
-    "rank": 6,
-    "is_new": false,
-    "is_active": true,
-    "type": "coin",
-    "price": 500
-  },
-  {
-    "id": "ada-cardano",
-    "name": "Cardano",
-    "symbol": "ADA",
-    "rank": 11,
-    "is_new": false,
-    "is_active": true,
-    "type": "coin",
-    "price": 400
-  },
-  {
-    "id": "miota-iota",
-    "name": "IOTA",
-    "symbol": "MIOTA",
-    "rank": 14,
-    "is_new": false,
-    "is_active": true,
-    "type": "coin",
-    "price": 300
-  },
-  {
-    "id": "neo-neo",
-    "name": "NEO",
-    "symbol": "NEO",
-    "rank": 17,
-    "is_new": false,
-    "is_active": true,
-    "type": "coin",
-    "price": 200
-  },
-  {
-    "id": "xem-nem",
-    "name": "NEM",
-    "symbol": "XEM",
-    "rank": 19,
-    "is_new": false,
-    "is_active": true,
-    "type": "coin",
-    "price": 100
+
+  sendMultipleRequests(urls, callback) {
+    let requestCount = urls.length;
+    let results = [];
+
+    urls.forEach(url => {
+      HttpService.sendRequest(url, data => {
+        results.push({ url, data });
+        requestCount--;
+
+        if (!requestCount) {
+          callback(results);
+        }
+      })
+    })
   }
-];
+};
 
 export const DataService = {
-  getCurrencies() {
-    return data;
+  getCurrencies(callback) {
+    HttpService.sendRequest(COINS_URL, data => {
+      data = data.slice(0, 10);
+      DataService.getCurrenciesPrices(data, callback);
+    });
+  },
+ 
+  getCurrenciesPrices(data, callback) {
+    let coinsIds = data.map(coin => coin.id);
+
+    const coinsIdMap = coinsIds.reduce((acc, id) => {
+      acc[getSingleCoinUrl(id)] = id;
+      return acc;
+    }, {});
+
+    HttpService.sendMultipleRequests(Object.keys(coinsIdMap), coins => {
+      const dataWithPrices = data.map(coinData => {
+        let coinPriceUrl = getSingleCoinUrl(coinData.id);
+        let [coindPriceData] = coins.find(coin => coin.url === coinPriceUrl).data;
+
+        coinData.price = coindPriceData.close;
+        return coinData;
+      });
+
+      callback(dataWithPrices);
+    })
+
+
+    // console.log(coinsIdMap)
   }
+
 }
